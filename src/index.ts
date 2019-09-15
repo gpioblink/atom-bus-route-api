a24429284e37cc8a2cd9b9a2807506d3bea391cf
gpioblink <gpioblink@aol.com>
2019-09-15 18:20:59 +0900

    read searchbiz api
    Former-commit-id: 11973c4d1c3401e3a7f87100a8e90ee78830b9b3

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import SearchQuery from '@/models/entities/SearchQuery';
 import * as bodyParser from 'body-parser';
+import SearchByBizAPI from '@/logics/api/SearchByBizAPI';
 const app = express();
 const port = 3000;
 
@@ -14,9 +15,9 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World! Please use /searchBusRoute'));
 
-app.post('/searchBusRoute', (req, res) => {
+app.post('/searchBusRoute', async (req, res) => {
   const query: SearchQuery = req.body;
-  res.send(req.body);
+  res.send(await SearchByBizAPI.searchRoute(query));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/logics/api/SearchByBizAPI.ts
+++ src/logics/api/SearchByBizAPI.ts
@@ -1,22 +1,20 @@
 import SearchQuery from '@/models/entities/SearchQuery';
 import * as moment from 'moment';
 
-export class SearchByBizAPI {
-  static searchRoute(query: SearchQuery) {
+export default class SearchByBizAPI {
+  static async searchRoute(query: SearchQuery) {
     const BASEURI = process.env.JOLDAN_ENDPOINT;
     const API_KEY = process.env.JOLDAN_BIZ_API_KEY;
     const FUNC_NAME = 'sr.cgi';
-    fetch(
+    const result = await fetch(
       `${BASEURI}/${FUNC_NAME}?f=1&ak=${API_KEY}&eki1=${query.from}&eki2=${query.to}&kbn1=B&kbn2=B&date=${moment(
         query.date
       ).format('YYYYMMDD')}`
-    ).then(res => {
-      const busStation = res.text;
-      return busStation;
-    });
+    );
+    const busStation = result.text;
+    return busStation;
   }
 
-  static searchTimeTable() {
-    
-  }
+  // static searchTimeTable() { 
+  // }
 }
