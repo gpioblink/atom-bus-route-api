b040884aa6a7afc436e056e4961de569c39c4623
gpioblink <gpioblink@aol.com>
2019-09-16 11:38:27 +0900

    return result
    Former-commit-id: 1302de3d0b247cf70ff8486d53032c3525611b3e

--- src/logics/api/SearchByBizAPI.ts
+++ src/logics/api/SearchByBizAPI.ts
@@ -1,20 +1,22 @@
 import SearchQuery from '@/models/entities/SearchQuery';
 import * as moment from 'moment';
 import axios from 'axios';
+import CommonOutput from '@/models/entities/jorudan/CommonOutput';
+import SearchRouteOutput from '@/models/entities/jorudan/SearchRouteOutput';
 
 export default class SearchByBizAPI {
   static async searchRoute(query: SearchQuery) {
     const BASEURI = process.env.JOLDAN_ENDPOINT;
     const API_KEY = process.env.JOLDAN_BIZ_API_KEY;
     const FUNC_NAME = 'sr.cgi';
-    const REQUEST_URL = `${BASEURI}/${FUNC_NAME}?f=1&ak=${API_KEY}&eki1=${encodeURI(query.from)}&eki2=${encodeURI(query.to)}&kbn1=B&kbn2=B&opt3=3&opt4=1&rm=TasyaBus=On&date=${encodeURI(moment(
-                          query.date
-                        ).format('YYYYMMDD'))}`;
-    console.log(REQUEST_URL);
+    const REQUEST_URL = `${BASEURI}/${FUNC_NAME}?f=1&ak=${API_KEY}&eki1=${encodeURI(query.from)}&eki2=${encodeURI(
+      query.to
+    )}&kbn1=B&kbn2=B&opt3=3&opt4=1&rm=TasyaBus=On&date=${encodeURI(moment(query.date).format('YYYYMMDD'))}`;
+    console.log(`GET: ${REQUEST_URL}`);
     const result = await axios.get(REQUEST_URL);
-    console.log(result);
-    const busStation = result.data.explanation;
-    return busStation;
+    const apiResult = result.data as CommonOutput;
+    const apiBody = apiResult.NorikaeBizApiResult.body as SearchRouteOutput;
+    return apiBody;
   }
 
   // static searchTimeTable() { 
