f11c90230af61acbc866deb956ac454d48bb7e34
gpioblink <gpioblink@aol.com>
2019-09-17 00:53:35 +0900

    use allow
    Former-commit-id: 60bc2af563a22a927dfa1e9a048eaa29a386e48e

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,9 @@ app.use(
 
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
