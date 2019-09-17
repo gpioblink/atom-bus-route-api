e7c16356de477e810bf4f2bb8eb20b769683aa16
gpioblink <gpioblink@aol.com>
2019-09-17 19:24:16 +0900

    add all results
    Former-commit-id: 470d461b184ce9388d20e52f15fa613889ea620c

--- src/logics/api/SearchBusRouteBeyondTheCompany.ts
+++ src/logics/api/SearchBusRouteBeyondTheCompany.ts
@@ -2,17 +2,38 @@ import SearchQuery from '@/models/entities/SearchQuery';
 import SearchByBizAPI from '@/logics/api/SearchByBizAPI';
 import Grid from '@/models/entities/Grid';
 import GeocodingAPI from '@/logics/api/GeocodingAPI';
+import { Route } from '@/models/entities/Route';
 
 export default class SearchBusRouteBeyondTheCompany {
   static async searchRoute(query: SearchQuery) {
     const fullFromList = await SearchBusRouteBeyondTheCompany.findFullStationNameList(query.from);
     const fullToList = await SearchBusRouteBeyondTheCompany.findFullStationNameList(query.to);
-    const fixedQuery = { from: fullFromList[0], to: fullToList[0], date: query.date };
-    console.log(fixedQuery);
-    return SearchByBizAPI.searchRoute(fixedQuery);
+    return SearchBusRouteBeyondTheCompany.execBulkRouteSearch(fullFromList, fullToList, query.date);
   }
 
-  static async execBulkRouteSearch(inputName: string) {}
+  static async execBulkRouteSearch(fromList: string[], toList: string[], date: Date) {
+    let routeList: Route[] = [];
+    for (const from of fromList) {
+      for (const to of toList) {
+        const query = { from, to, date };
+        console.log(query);
+        routeList = routeList.concat(await SearchByBizAPI.searchRoute(query));
+      }
+    }
+
+    // 重複判定のためキーで管理。価格・時刻・距離が全て同じルートを削除
+    const routeObj: any = {};
+    for (const route of routeList) {
+      routeObj[`${route.price}/${route.time}/${route.distance}`] = route;
+    }
+    routeList = [];
+    for (const key in routeObj) {
+      routeList.push(routeObj[key]);
+    }
+
+    console.log(`total ${routeList.length} results found`);
+    return routeList;
+  }
 
   static async findFullStationNameList(inputName: string): Promise<string[]> {
     const try1 = await SearchByBizAPI.getFullBusStationNameListByPlaceName(inputName);
